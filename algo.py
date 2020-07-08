e8d9bbaa42200a4f8ddc42a6bb9e795478e90f37
minipele06 <aja398@stern.nyu.edu>
2020-07-08 17:36:04 -0400

    connection to Alpaca API complete

--- algo.py
+++ algo.py
@@ -1,8 +1,14 @@
 #Trading algo test
-
+from dotenv import load_dotenv
 import alpaca_trade_api as tradeapi
+import os
 
-api = tradeapi.REST('<key_id>', '<secret_key>', api_version='v2') # or use ENV Vars shown below
+load_dotenv()
+
+API_KEY = os.getenv("APCA_API_KEY_ID")
+API_SECRET_KEY = os.getenv("APCA_API_SECRET_KEY")
+
+api = tradeapi.REST(API_KEY, API_SECRET_KEY, api_version='v2') # or use ENV Vars shown below
 account = api.get_account()
 
-print(account)
+print(account.status)
